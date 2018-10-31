cc80b92b7e0a7ba21cdd8065e989b9029d091582
olero <43321114+olero@users.noreply.github.com>
2018-10-31 02:38:34 -0400

    Updated to fix mistake

--- crime_map_overlay_attributed_nodes.py
+++ crime_map_overlay_attributed_nodes.py
@@ -7,7 +7,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KDTree
 
-
 # Get all the crime data from the .csv file and isolate the points based on the latitudes and longtiudes
 data = pd.read_csv(r'interventionscitoyendo.csv', encoding='ISO-8859-1')
 inSet = data[(data.LAT <= 45.512855) & (data.LAT >= 45.502971) & (data.LONG >= -73.583204) & (data.LONG <= -73.570142)]
@@ -22,10 +21,11 @@ nodes, _ = ox.graph_to_gdfs(G)
 # Create a k-d tree based on the GeoDataFrame of the map
 tree = KDTree(nodes[['y', 'x']], metric='euclidean')
 
+
 # Loop through all the data for the area and attributing the latitude and longtiude of each of the
 # crimes to the nearest intersection node of the map
 for i in range(len(inSet.LAT)):
-    crime_idx = tree.query([inSet.LAT.values[i],inSet.LONG.values[i]], k=1, return_distance=False)[0]
+    crime_idx = tree.query([(inSet.LAT.values[i],inSet.LONG.values[i])], k=1, return_distance=False)[0]
     closest_node_to_crime = nodes.iloc[crime_idx].index.values[0]
     ax.scatter(G.node[closest_node_to_crime]['x'], G.node[closest_node_to_crime]['y'], c='red', s=3)
 
